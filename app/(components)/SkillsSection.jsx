7c5f1aab8d1eb636ce9048487fc4c8da70cc55d2
Nestor162 <nestorcicardini@gmail.com>
2023-09-08 14:00:06 +0200

    Fix responsiveness and spacing

--- app/(components)/SkillsSection.jsx
+++ app/(components)/SkillsSection.jsx
@@ -105,41 +105,46 @@ function SkillsSection() {
   return (
     <article className='mt-28 ms-0 md:ms-9'>
       <div
-        className={`${chakra.className} relative w-5/6 md:w-3/5 text-xl mx-auto space-y-4 mb-6 border border-gray-600 rounded py-3 px-10 md:py-5 md:px-12 mt-32 md:mt-40`}
+        className={`${chakra.className} relative w-5/6 md:w-3/5 text-xl mx-auto mb-6 border border-gray-600 rounded py-3 px-7 md:py-5 md:px-12 mt-32 md:mt-40 shadow-lg`}
       >
         <h2 className={`${michroma.className} absolute -top-5 left-4 text-2xl bg-white px-3`}>Skills</h2>
         <span id='skills' className='absolute -top-28 w-14 h-8'></span>
-        <h4 className='hidden xl:block absolute -rotate-90 top-10 start-0'>Frontend</h4>
 
-        <div className='flex justify-center items-center gap-4 flex-wrap'>
+        <h4 className='hidden xl:block absolute -rotate-90 top-16 start-0'>Frontend</h4>
+
+        <h4 className='block xl:hidden text-center mt-7 sm:mt-14'>Frontend</h4>
+
+        <div className='flex justify-center items-center gap-4 flex-wrap mt-3 sm:mt-7 '>
           {skillsFront.map(skill => (
             <figure key={skill.name} className='flex flex-col justify-center items-center'>
               <Image src={skill.src} alt={skill.alt} width={50} height={50} />
-              <figcaption>{skill.name}</figcaption>
+              <figcaption className='text-sm sm:text-xl'>{skill.name}</figcaption>
             </figure>
           ))}
         </div>
 
-        <h4 className='hidden xl:block absolute -rotate-90 top-44 start-0'>Backend</h4>
+        <h4 className='hidden xl:block absolute -rotate-90 top-52 start-0'>Backend</h4>
+        <h4 className='block xl:hidden text-center mt-14'>Backend</h4>
 
-        <div className='flex justify-center items-center gap-4 flex-wrap' style={{ marginTop: '50px' }}>
+        <div className='flex justify-center items-center gap-4 flex-wrap mt-3 sm:mt-14'>
           {skillsBack.map(skill => (
             <figure key={skill.name} className='flex flex-col justify-center items-center'>
               <Image src={skill.src} alt={skill.alt} width={50} height={50} />
-              <figcaption>{skill.name}</figcaption>
+              <figcaption className='text-sm sm:text-xl'>{skill.name}</figcaption>
             </figure>
           ))}
         </div>
 
-        <h4 className='hidden xl:block absolute -rotate-90 start-3' style={{ top: '19rem' }}>
+        <h4 className='hidden xl:block absolute -rotate-90 start-3' style={{ top: '21.5rem' }}>
           Tools
         </h4>
+        <h4 className='block xl:hidden text-center mt-14'>Tools</h4>
 
-        <div className='flex justify-center items-center gap-4 flex-wrap' style={{ marginTop: '50px' }}>
+        <div className='flex justify-center items-center gap-4 flex-wrap sm:mt-14 mt-3 mb-7'>
           {tools.map(skill => (
             <figure key={skill.name} className='flex flex-col justify-center items-center'>
               <Image src={skill.src} alt={skill.alt} width={50} height={50} />
-              <figcaption>{skill.name}</figcaption>
+              <figcaption className='text-sm sm:text-xl'>{skill.name}</figcaption>
             </figure>
           ))}
         </div>
