c79d262b467edda17eb56753c9d009259ce39659
Nestor162 <nestorcicardini@gmail.com>
2023-09-13 18:00:03 +0200

    Add dark/light mode toggler component

---
+++ app/(components)/ThemeToggler.jsx
@@ -0,0 +1,29 @@
+'use client'
+import React, { useState } from 'react'
+
+function ThemeToggler() {
+  const [darkMode, setDarkMode] = useState(false)
+
+  return (
+    <div className='relative inline-block w-10 align-middle select-none transition duration-200 ease-in'>
+      <input
+        type='checkbox'
+        name='toggle'
+        id='toggle'
+        checked={darkMode}
+        onChange={() => setDarkMode(!darkMode)}
+        className={`toggle-checkbox absolute -top-[6px] block w-6 h-6 rounded-full border-2 border-gray-100 appearance-none cursor-pointer transform transition-transform duration-200 ease-in ${
+          darkMode ? 'translate-x-4 bg-cyan-700' : 'bg-yellow-400'
+        }`}
+      />
+      <label
+        htmlFor='toggle'
+        className={`toggle-label block overflow-hidden h-3 rounded-full cursor-pointer ${
+          darkMode ? 'bg-gray-800' : 'bg-gray-300'
+        }`}
+      ></label>
+    </div>
+  )
+}
+
+export default ThemeToggler
