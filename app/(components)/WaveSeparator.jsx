86e923b7c4a94a33e80ad16a752d6b3611c36032
Nestor162 <nestorcicardini@gmail.com>
2023-09-14 16:46:42 +0200

    Add wave separator

---
+++ app/(components)/WaveSeparator.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './WaveSeparator.css'
+
+function WaveSeparator() {
+  return (
+    <section className=' absolute w-screen h-[800px] sm:h-[620px] top-0 left-0 right-0 bg-slate-100 dark:bg-slate-700 -z-10'>
+      <div className='wave absolute h-[70px] w-full bottom-0 bg-slate-100 dark:bg-slate-700'></div>
+    </section>
+  )
+}
+
+export default WaveSeparator
