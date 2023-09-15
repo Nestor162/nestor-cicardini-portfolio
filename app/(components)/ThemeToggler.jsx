7ba653694f01588fc65e22ad3ff3ade24dde135e
Nestor162 <nestorcicardini@gmail.com>
2023-09-15 12:26:48 +0200

    Add functionality to theme toggler

--- app/(components)/ThemeToggler.jsx
+++ app/(components)/ThemeToggler.jsx
@@ -1,8 +1,12 @@
 'use client'
+import { useTheme } from 'next-themes'
 import React, { useState } from 'react'
 
 function ThemeToggler() {
-  const [darkMode, setDarkMode] = useState(false)
+  const { theme, setTheme, resolvedTheme } = useTheme()
+  const [darkMode, setDarkMode] = useState(theme === 'dark' || resolvedTheme === 'dark')
+
+  console.log(resolvedTheme, theme)
 
   return (
     <div className='relative inline-block w-10 align-middle select-none transition duration-200 ease-in'>
@@ -11,15 +15,18 @@ function ThemeToggler() {
         name='toggle'
         id='toggle'
         checked={darkMode}
-        onChange={() => setDarkMode(!darkMode)}
+        onChange={() => {
+          setDarkMode(!darkMode)
+          setTheme(darkMode ? 'dark' : 'light')
+        }}
         className={`toggle-checkbox absolute -top-[6px] block w-6 h-6 rounded-full border-2 border-gray-100 appearance-none cursor-pointer transform transition-transform duration-200 ease-in ${
-          darkMode ? 'translate-x-4 bg-cyan-700' : 'bg-yellow-400'
+          !darkMode ? 'translate-x-4 bg-cyan-700' : 'bg-yellow-400'
         }`}
       />
       <label
         htmlFor='toggle'
         className={`toggle-label block overflow-hidden h-3 rounded-full cursor-pointer ${
-          darkMode ? 'bg-gray-800' : 'bg-gray-300'
+          !darkMode ? 'bg-gray-800' : 'bg-gray-300'
         }`}
       ></label>
     </div>
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Providers from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +16,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html className='scroll-smooth dark' lang='en'>
-      <body className={`${inter.className} bg-white dark:bg-slate-800 dark:text-slate-300`}>{children}</body>
+    <html className='scroll-smooth' lang='en'>
+      <body className={`${inter.className} bg-white dark:bg-slate-800 dark:text-slate-300`}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
