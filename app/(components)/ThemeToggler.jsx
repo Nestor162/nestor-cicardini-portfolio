5316573c1ea63d8106a6339bc4ba051f9af90a08
Nestor162 <nestorcicardini@gmail.com>
2023-09-15 16:57:31 +0200

    Add default toggler style when page is loading

--- app/(components)/ThemeToggler.jsx
+++ app/(components)/ThemeToggler.jsx
@@ -3,14 +3,34 @@ import React, { useEffect, useState } from 'react'
 
 function ThemeToggler() {
   const { theme, setTheme, resolvedTheme } = useTheme()
-  const [isDark, setIsDark] = useState(false)
+  const [isDark, setIsDark] = useState(null) // Start with null when component is loading
 
   useEffect(() => {
     setIsDark(resolvedTheme === 'dark')
   }, [resolvedTheme])
 
   const toggleTheme = () => {
-    setTheme(isDark ? 'light' : 'dark')
+    if (isDark !== null) {
+      setTheme(isDark ? 'light' : 'dark')
+    }
+  }
+
+  // If it's loading show the toggler default's style
+  if (isDark === null) {
+    return (
+      <div className='relative inline-block w-10 align-middle select-none transition duration-200 ease-in'>
+        <input
+          type='checkbox'
+          name='toggle'
+          id='toggle'
+          className='toggle-checkbox absolute -top-[6px] block w-6 h-6 rounded-full border-2 border-gray-100 appearance-none cursor-pointer transform transition-transform duration-200 ease-in bg-slate-300 translate-x-2'
+        />
+        <label
+          htmlFor='toggle'
+          className='toggle-label block overflow-hidden h-3 rounded-full cursor-pointer bg-gray-300'
+        ></label>
+      </div>
+    )
   }
 
   return (
