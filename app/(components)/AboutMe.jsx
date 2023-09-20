b3b2df2ddcf1273c3d08961b2cc093565c6526b4
Nestor162 <nestorcicardini@gmail.com>
2023-09-20 09:12:49 +0200

    Change static subtitle with dynamic value

--- app/(components)/AboutMe.jsx
+++ app/(components)/AboutMe.jsx
@@ -8,7 +8,7 @@ function AboutMe({ content }) {
         className={`${chakra.className} relative w-5/6 md:w-3/5 text-xl mx-auto space-y-4 mb-6 border border-gray-600 dark:border-slate-400 rounded pt-3 pb-7 px-10 md:py-5 md:px-12 mt-32 md:mt-40 shadow-lg`}
       >
         <h2 className={`${michroma.className} absolute -top-5 left-4 text-2xl bg-white dark:bg-slate-800 px-3`}>
-          About Me
+          {content.title}
         </h2>
         <span id='about' className='absolute -top-28 w-14 h-8'></span>
 
