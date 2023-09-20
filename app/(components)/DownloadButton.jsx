2e5f229776526f4620896f53e175381ccc6c768f
Nestor162 <nestorcicardini@gmail.com>
2023-09-20 09:25:21 +0200

    Optimize link and set target 'new tab'

--- app/(components)/DownloadButton.jsx
+++ app/(components)/DownloadButton.jsx
@@ -1,21 +1,18 @@
 'use client'
+import Link from 'next/link'
 import { usePathname, useRouter } from 'next/navigation'
 
 export default function DownloadButton({ content }) {
-  const router = useRouter()
   const path = usePathname()
   const lang = path.split('/')[1]
   return (
-    <a
-      // href={`/CV/Nestor_Cicardini_CV_one_page_${content.cvLang}.pdf`}
-      onClick={() => {
-        const filePath = `${lang}/CV/Nestor_Cicardini_CV_one_page_${content.cvLang}.pdf`
-        router.push(filePath)
-      }}
+    <Link
+      href={`${lang}/CV/Nestor_Cicardini_CV_one_page_${content.cvLang}.pdf`}
       target='_blank'
+      rel='noopener noreferrer'
       className='neon-border-button ms-1 flex justify-center sm:inline-block'
     >
       {content.button}
-    </a>
+    </Link>
   )
 }
